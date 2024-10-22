f3347f39568eb69a2bfe4be70ddb8ee3e14aa1ee
David Castillo Mora <davidcm2803@gmail.com>
2024-10-21 22:06:03 -0600

    Initial commit

--- Ulacit_parkingC#/App_Start/BundleConfig.cs
+++ Ulacit_parkingC#/App_Start/BundleConfig.cs
@@ -17,14 +17,16 @@ namespace Ulacit_parkingC_
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*", "~/Content/js/demo/chart-pie-demo.js", "~/Content/js/demo/chart-area-demo.js", "~/Content/vendor/chart.js/Chart.min.js",
+                        "~/Content/js/sb-admin-2.min.js", "~/Content/vendor/jquery-easing/jquery.easing.min.js", "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                        "~/Content/vendor/jquery/jquery.min.js"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", "~/Content/css/sb-admin-2.min.css", "~/Content/vendor/fontawesome-free/css/all.min.css"));
         }
     }
 }
--- Ulacit_parkingC#/App_Start/RouteConfig.cs
+++ Ulacit_parkingC#/App_Start/RouteConfig.cs
@@ -20,4 +20,5 @@ namespace Ulacit_parkingC_
             );
         }
     }
+
 }
